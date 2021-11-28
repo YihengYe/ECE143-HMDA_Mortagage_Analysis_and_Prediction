1afd6336ca824c24fee64e58dd3386405fcd3648
YihengYe <yiy291@ucsd.edu>
2021-11-27 17:23:45 -0800

    total gender percentage

--- src/visualizations/Gender_analysis.py
+++ src/visualizations/Gender_analysis.py
@@ -42,7 +42,8 @@ def Gender_coding(code_map):
     :return: The updated gender code map
     """
     gender_dict=code_map['sex'].copy()
-    gender_dict[3]='Information not provided by applicant'
+    gender_dict[3]='Unknown'
+    gender_dict[4]='Unknown'
     return gender_dict
 
 def Generate_Gender_Plots(df,genderMap, parent_path, image_path):
@@ -58,12 +59,40 @@ def Generate_Gender_Plots(df,genderMap, parent_path, image_path):
         os.makedirs(image_path)
     
     
-    Sex_total=df['applicant_sex'].value_counts()
+    Sex_total=df['applicant_sex'].replace(genderMap).value_counts()
     # Draw Gender composition for All applicants
     plt.figure()
-    Sex_total=Sex_total.rename(index=genderMap)
     Sex_total.plot.pie(legend=False, title='Gender Composition for All Applicants',autopct='%1.1f%%')
     plt.savefig(image_path+'Total_applicant_gender.png', bbox_inches='tight')
+    plt.close()
+
+    # Making analysis on applicant sex vs approval 
+    Sex_result=df[['applicant_sex','action_taken']]
+    RecodeAction={1:"Institution approved",2:"Institution approved", 3:'Institution denied', 7:'Institution denied',8:'Institution approved'}
+    Sex_result_b=pd.DataFrame()
+    Sex_result_b['action']= Sex_result['action_taken'].replace(RecodeAction)
+    Sex_result_b['applicant_sex']=Sex_result['applicant_sex'].replace(genderMap)
+    Sex_result_a1=Sex_result_b.pivot_table(index='applicant_sex', columns='action',aggfunc='size',fill_value=0)
+    plt.figure() #Gender application resultcount
+    Sex_result_a1.plot.bar(title='Application Result Counts vs Gender')
+    plt.xticks(rotation=45)
+    plt.ylabel('count')
+    plt.savefig(image_path+'Application_result_count_gender.png', bbox_inches='tight')
+    plt.close()
+
+    totalSA1=Sex_result_a1.sum(axis=1)
+    Sex_result_a2=Sex_result_a1.T
+    temp=Sex_result_a2/totalSA1.values
+    Sex_result_a2=temp.T*100
+    plt.figure()
+    SA2B=Sex_result_a2.plot.bar(title='Application Result Precentage vs Gender')
+    for p in SA2B.patches:
+        SA2B.annotate(str("%.2f"%p.get_height())+'%', xy=(p.get_x(), p.get_height()*1.01))
+    plt.xticks(rotation=360)
+    plt.ylabel('percentage')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.savefig(image_path+'Application_result_percentage_gender.png', bbox_inches='tight')
+    plt.close()
     return
 
 if __name__ == "__main__":
